b6de618cf50b1aed2a5737d7dd56e75f8863b5e9
syafiqmk <syafiqmuh.k@gmail.com>
2024-04-27 21:43:55 +0800

    Update AuthController Create login, attempt, and logout Function, Update web Route add login, attempt, and logout route

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,5 +6,18 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    //
+    // Login page
+    public function login() {
+
+    }
+
+    // Login Attempt Process
+    public function attempt() {
+
+    }
+
+    // Logout Process
+    public function logout() {
+        
+    }
 }
