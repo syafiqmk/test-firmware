f97707a9032122ca320799706bfc41fa3566912c
syafiqmk <syafiqmuh.k@gmail.com>
2024-04-27 21:52:47 +0800

    Finish all Functions function

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,21 +3,37 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
     // Login page
     public function login() {
-
+        return view('auth.login');
     }
 
     // Login Attempt Process
-    public function attempt() {
+    public function attempt(Request $request) {
+        $validate = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
 
+        if(Auth::attempt($validate)) {
+            $request->session()->regenerate();
+            return redirect()->route('web.firmware');
+        } else {
+            return redirect()->route('auth.login')->with('danger', 'Fail to Login check your Email & Password!');
+        }
     }
 
     // Logout Process
-    public function logout() {
-        
+    public function logout(Request $request) {
+        auth()->logout();
+
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return redirect()->route('/');
     }
 }
