1a59bf7f0d59d4f7d9b99aa1a9e2d2f4c8ebd20e
syafiqmk <syafiqmuh.k@gmail.com>
2024-04-27 21:11:34 +0800

    Add guarded variable

--- app/Models/DeviceCategory.php
+++ app/Models/DeviceCategory.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class DeviceCategory extends Model
 {
     use HasFactory;
+
+    protected $guarded = ['id'];
 }
--- app/Models/Firmware.php
+++ app/Models/Firmware.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Firmware extends Model
 {
     use HasFactory;
+
+    protected $guarded = ['id'];
 }
