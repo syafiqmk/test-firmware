d4be161d82f8b325ac75f033d835bb7605c27aa2
syafiqmk <syafiqmuh.k@gmail.com>
2024-04-28 21:13:59 +0800

    Add relationship to all Models

--- app/Models/Brand.php
+++ app/Models/Brand.php
@@ -2,12 +2,19 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Firmware;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Brand extends Model
 {
     use HasFactory;
 
     protected $guarded = ['id'];
+
+    // Relation to Firmware Model
+    public function firmware(): HasMany {
+        return $this->hasMany(Firmware::class);
+    }
 }
--- app/Models/Firmware.php
+++ app/Models/Firmware.php
@@ -4,10 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Firmware extends Model
 {
     use HasFactory;
 
     protected $guarded = ['id'];
+
+    // Relation to Brand Model
+    public function brand(): BelongsTo {
+        return $this->belongsTo(Brand::class);
+    }
+
+    // Relation to DeviceCategory Model
+    public function deviceCategory(): BelongsTo {
+        return $this->belongsTo(DeviceCategory::class);
+    }
 }
